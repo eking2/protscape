21864539853f7ccabb235f0244d36d41d33778e7
eking2 <eking2@uci.edu>
2021-09-07 14:04:22 -0700

    add tape datasets and lit model

---
+++ protscape/datasets.py
@@ -0,0 +1,143 @@
+from Bio import SeqIO
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+import pytorch_lightning as pl
+from typing import Union, Optional, Tuple
+from pathlib import Path
+import tarfile
+import pandas as pd
+import numpy as np
+from protscape.utils import seq_to_ohe, ohe_to_seq, download_tape, download_envision
+
+
+class TAPEDataset(Dataset):
+    def __init__(self, json_path: Union[str, Path], dataset: str, pad: int) -> None:
+
+        if dataset == "stability":
+            target = "stability_score"
+        elif dataset == "fluorescence":
+            target = "log_fluorescence"
+
+        self.df = pd.read_json(json_path)
+        self.seqs = self.df["primary"].values
+
+        self.X = np.array([seq_to_ohe(seq, pad=pad) for seq in self.seqs])
+        self.y = self.df[target].apply(lambda x: x[0]).values
+
+    def __len__(self) -> int:
+        return len(self.y)
+
+    def __getitem__(self, idx: int) -> Tuple[np.ndarray, float]:
+        return self.X[idx], self.y[idx]
+
+
+class TAPEDataModule(pl.LightningDataModule):
+    def __init__(
+            self, dataset: str, pad: int, data_dir: Union[str, Path] = "./data", batch_size: int = 64,
+    ) -> None:
+        super().__init__()
+
+        self.dataset = dataset
+        self.pad = pad
+        self.data_dir = Path(data_dir)
+        self.batch_size = batch_size
+
+    def prepare_data(self) -> None:
+
+        """Download TAPE json dataset."""
+
+        download_tape(dataset=self.dataset, data_dir=self.data_dir)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+
+        """Setup datasets for train, valid, and test splits."""
+
+        if stage == "fit":
+            json_path = Path(
+                f"{self.data_dir}/{self.dataset}/{self.dataset}_train.json"
+            )
+            self.tape_train = TAPEDataset(json_path=json_path, dataset=self.dataset, pad=self.pad)
+
+        if stage == "validation":
+            json_path = Path(
+                f"{self.data_dir}/{self.dataset}/{self.dataset}_valid.json"
+            )
+            self.tape_valid = TAPEDataset(json_path=json_path, dataset=self.dataset, pad=self.pad)
+
+        if stage == "test":
+            json_path = Path(f"{self.data_dir}/{self.dataset}/{self.dataset}_test.json")
+            self.tape_test = TAPEDataset(json_path=json_path, dataset=self.dataset, pad=self.pad)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.tape_train, batch_size=self.batch_size)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.tape_valid, batch_size=self.batch_size)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.tape_test, batch_size=self.batch_size)
+
+
+class EnvisionDataset(Dataset):
+    def __init__(self, fasta_path: Union[str, Path] = "./data/P62593.fasta"):
+
+        self.df = self.fasta_to_df(fasta_path)
+
+        self.X = np.array([seq_to_ohe(seq) for seq in self.df["seq"]])
+        self.y = self.df["activity"].values
+
+    def fasta_to_df(self, fasta_path):
+
+        # read fasta
+        records = SeqIO.parse(fasta_path, "fasta")
+
+        # get mutation and activity from record header, save seq
+        data = []
+        for record in records:
+            seq = str(record.seq)
+            _, sample, activity = record.name.split("|")
+            _, mutation = sample.split("_")
+
+            data.append([mutation, seq, activity])
+
+        # to dataframe
+        df = pd.DataFrame(data, columns=["mutation", "seq", "activity"])
+        df["activity"] = df["activity"].apply(pd.to_numeric, errors="ignore")
+
+        return df
+
+    def __len__(self) -> int:
+        return len(self.y)
+
+    def __getitem__(self, idx: int) -> Tuple[np.ndarray, float]:
+        return self.X[idx], self.y[idx]
+
+
+class EnvisionDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir: Union[str, Path], batch_size: int=64):
+
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.fasta_path = Path(data_dir, 'P62593.fasta')
+
+    def prepare_data(self):
+
+        download_envision(data_dir = self.data_dir)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+
+        self.envision = EnvisionDataset(fasta_path=self.fasta_path)
+
+        # 80 train/20 test split
+        n_train = int(0.8 * len(self.envision))
+        n_test = len(self.envision) - n_train
+
+        self.envision_train, self.envision_test = random_split(dataset=self.envision, 
+                                                               lengths=[n_train, n_test], 
+                                                               generator=torch.Generator().manual_seed(42))
+
+    def train_dataloader(self):
+        return DataLoader(self.envision_train, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.envision_test, batch_size=self.batch_size)
---
+++ protscape/models.py
@@ -0,0 +1,230 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import numpy as np
+import pandas as pd
+import pytorch_lightning as pl
+from protscape import datasets
+from sklearn.metrics import mean_squared_error
+from scipy.stats import spearmanr
+from typing import List, Dict, Tuple
+
+
+class MaxSeqPool(nn.Module):
+
+    """Layer to apply torch.max over sequence dimension."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.max(x, dim=-1)[0]
+
+
+class CNNEncoder(pl.LightningModule):
+
+    """Landscape prediction from one-hot protein sequences.
+
+    Parameters 
+    ----------
+    learning_rates : List[float]
+        List of learning rates for the encoder, mapper, and predictor components
+    weight_decays : List[float]
+        List of weight decay values for the encoder, mapper, and predictor components
+    reduction : str
+        Pooling operation to apply on CNN encodings 
+    encoder_kwargs : Dict
+        Options for CNN encoder
+
+    Attributes
+    ----------
+    encoder : nn.Sequential
+        Generally a 1-3 layers CNN that converts a one-hot sequence to embedding
+    mapper : nn.Sequential
+        Pooling operation, either maxpool (get max on sequence length dimension of embedding) 
+        or linear_maxpool (linear layer then get max on seq dim). Results in fixed length output, 
+        not a regular pooling operation since its not a sliding window on the sequence.
+    predictor : nn.Sequential
+        Single fully connected layer taking pooled embedding to activity prediction
+
+    Steps
+    -----
+    End-to-end optimization of OHE seq -> CNN encoder -> mapper (pooling operation) -> predictor
+    """
+
+    def __init__(
+        self,
+        learning_rates: List,
+        weight_decays: List,
+        reduction: str,
+        encoder_kwargs: Dict,
+    ) -> None:
+
+        super().__init__()
+
+        self.learning_rates = learning_rates
+        self.weight_decays = weight_decays
+        self.reduction = reduction
+
+        encoder_out = encoder_kwargs["out_channels"][-1]
+
+        self.encoder = nn.Sequential(*self.make_encoder(encoder_kwargs))
+        self.mapper = nn.Sequential(*self.make_mapper(reduction, encoder_out))
+        self.predictor = nn.Sequential(*self.make_predictor(reduction))
+
+    def make_encoder(self, encoder_kwargs: Dict) -> List:
+
+        """From one-hot to CNN encoded representation."""
+
+        conv_layers = encoder_kwargs["layers"]
+        out_channels = encoder_kwargs["out_channels"]
+        kernel_sizes = encoder_kwargs["kernel_sizes"]
+        dilations = encoder_kwargs["dilations"]
+
+        encoder_layers = []
+
+        for i in range(conv_layers):
+
+            # 22 valid characters (20 amino acids + gap + unknown)
+            in_chan = 22 if i == 0 else out_channels[i - 1]
+
+            out_chan = out_channels[i]
+            kernel_size = kernel_sizes[i]
+            dilation = dilations[i]
+
+            # same padding (kernel -1)/2
+            pad = kernel_size // 2
+
+            layer = nn.Conv1d(
+                in_channels=in_chan,
+                out_channels=out_chan,
+                kernel_size=kernel_size,
+                padding=pad,
+                dilation=dilation,
+            )
+
+            encoder_layers.append(layer)
+            encoder_layers.append(nn.ReLU(inplace=True))
+
+        return encoder_layers
+
+    def make_mapper(self, reduction: str, in_chan: int) -> List:
+
+        """Pool the CNN encoder output."""
+
+        mapper_layers = []
+
+        if reduction == "linear_maxpool":
+            layer = nn.Linear(in_chan, 2048)
+            mapper_layers.append(layer)
+            mapper_layers.append(nn.ReLU(inplace=True))
+
+        mapper_layers.append(MaxSeqPool())
+
+        return mapper_layers
+
+    def make_predictor(self, reduction: str) -> List:
+
+        """Make activity prediction from embedding."""
+
+        predictor_layers = []
+
+        # maxpool out is 1024, linear_maxpool out is 2048
+        in_chan = 2048 if reduction == "linear_maxpool" else 1024
+
+        layer = nn.Linear(in_chan, 1)
+        predictor_layers.append(layer)
+
+        return predictor_layers
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        x = self.encoder(x)
+        x = self.mapper(x)
+        x = self.predictor(x)
+
+        return x
+
+    def configure_optimizers(self):
+
+        # set separate learning rates/weight decays for the encoder, mapper, and predictor
+        optimizer = torch.optim.Adam(
+            [
+                {
+                    "params": model.encoder.parameters(),
+                    "lr": self.learning_rates[0],
+                    "weight_decay": self.weight_decays[0],
+                },
+                {
+                    "params": model.mapper.parameters(),
+                    "lr": self.learning_rates[1],
+                    "weight_decay": self.weight_decays[1],
+                },
+                {
+                    "params": model.predictor.parameters(),
+                    "lr": self.learning_rates[2],
+                    "weight_decay": self.weight_decays[2],
+                },
+            ]
+        )
+
+        return optimizer
+
+    def training_step(
+        self, train_batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
+
+        x, y = train_batch
+
+        # from (batch, seq length, charset) -> (batch, charset, seq length)
+        x = x.permute(0, 2, 1).float()
+        y = y.unsqueeze(-1).float()
+
+        preds = self(x)
+
+        loss = F.mse_loss(preds, y)
+        self.log("train_loss", loss)
+
+        return loss
+
+    def test_step(
+        self, test_batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
+    ) -> torch.Tensor:
+
+        x, y = test_batch
+        x = x.permute(0, 2, 1).float()
+        y = y.unsqueeze(-1).float()
+
+        preds = self(x)
+
+        loss = F.mse_loss(preds, y)
+        self.log("test_loss", loss)
+
+        return loss
+
+    def run_predict(
+        self,
+        dataloader: DataLoader,
+        y_min: Optional[float] = None,
+        y_max: Optional[float] = None,
+    ) -> Tuple[List, List]:
+
+        """Predict activity on input dataloader."""
+
+        model.eval()
+
+        preds = []
+        true = []
+
+        with torch.no_grad():
+            for batch in dataloader:
+                x, y = batch
+                x = x.permute(0, 2, 1).float()
+
+                pred = self(x).reshape(-1)
+
+                preds.extend(pred.detach().cpu().numpy())
+                true.extend(y.numpy())
+
+        return preds, true
